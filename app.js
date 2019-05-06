6172a86b6e4523c2471e02a5fe8752a94c2d705f
Seleznev Stas <seleznev.stas@gmail.com>
2019-05-06 15:32:07 +0300

    wip

---
+++ app.js
@@ -0,0 +1,6 @@
+const display = document.getElementById("display");
+
+window.addEventListener("deviceorientationabsolute", function(e) {    
+    display.textContent = `alpha=${e.alpha}  beta=${e.beta}  gamma=${e.gamma}`;
+    console.log(e)
+});
